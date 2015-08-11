c722700c789493a8652a5534a1b7661d642fa8f4
sam bassett <samson.bassett@gmail.com>
2015-08-11 13:57:52 -0700

    move resources to match code namespace, and delete unused images

--- src/main/java/com/lothrazar/mimic18/ModSamsContent.java
+++ src/main/java/com/lothrazar/mimic18/ModSamsContent.java
@@ -43,7 +43,7 @@ public class ModSamsContent
 	public static ModSamsContent instance;
 	public static Logger logger;
 	public final static String MODID = "mimic18";
-	public static String TEXTURE_LOCATION = "samspowerups:";
+	public static String TEXTURE_LOCATION = MODID+":";
 	public static final String VERSION = "1";
 	public static final String NAME = "Sam's Content";
 
